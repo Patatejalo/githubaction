fedf5812a5ca7e212404d39891c63b7effd797c1
Patatejalo <dl781002005@gmail.com>
2026-09-07 10:16:54 +0200

    Finalisation du TP GitHub Actions

--- job.py
+++ job.py
@@ -1,3 +1,12 @@
+import os
+
 a = 2
 
 print("coucou", a)
+
+secret = os.getenv("SECRET_API_TOKEN")
+
+if secret:
+    print("Le secret est bien accessible dans Python")
+else:
+    print("Le secret n'est pas accessible")
